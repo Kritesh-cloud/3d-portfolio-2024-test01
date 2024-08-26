9fcdd3ab9a1ef4edf1f5629e1938014d39968987
kritesh2nd <kritesh2nd@gmail.com>
2024-08-26 20:59:12 +0545

    fix: some bug

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kriteshthapa.com.np/"
+  base: "/3d-portfolio-2024-test01/",
+  // base: "/kriteshthapa.com.np/",
 })
